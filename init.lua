dd6fdd5ae5d2309ec34e125916ce77d519e61ae2
snail <snail1704@gmail.com>
2024-07-17 07:32:36 +0400

    Add ThePrimeagen remap `<leader>pv`

--- init.lua
+++ init.lua
@@ -818,6 +818,7 @@ require('lazy').setup({
 })
 
 vim.cmd [[colorscheme tokyonight-moon]]
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
